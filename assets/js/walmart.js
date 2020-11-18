8d100b4537cfdb6ef37d81030e44840e7af14077
Van Taymany <vtaymany@gmail.com>
2020-11-17 22:33:47 -0800

    returns availability

--- assets/js/walmart.js
+++ assets/js/walmart.js
@@ -5,19 +5,32 @@
 var zipCode = 91367
 var radius = 20
 
-var queryURL =
+var storeIDQueryURL =
   'https://www.walmart.com/store/finder/electrode/api/stores?singleLineAddr=' +
   zipCode +
   '&serviceTypes=pharmacy&distance=' +
   radius
 
 $.ajax({
-  url: queryURL,
+  url: storeIDQueryURL,
   method: 'GET',
 }).then(function (response) {
   var returnedStores = response.payload.storesData.stores
   for (i = 0; i < returnedStores.length; i++) {
     var storeID = returnedStores[i].id
     console.log(storeID)
+    var upc = 681131381284
+    var productQueryURL =
+      'https://search.mobile.walmart.com/v1/products-by-code/UPC/' +
+      upc +
+      '?storeId=' +
+      storeID
+    $.ajax({
+      url: productQueryURL,
+      method: 'GET',
+    }).then(function (response) {
+      var availability = response.data.inStore.inventory
+      console.log(availability)
+    })
   }
 })
