c188ac21f6abf6a4c6db103418cce6b9ce569a80
Van Taymany <vtaymany@gmail.com>
2020-11-17 22:16:15 -0800

    returns storeIDs

--- assets/js/walmart.js
+++ assets/js/walmart.js
@@ -1,6 +1,9 @@
 // The below code fills in the first row of the table
-var zipCode = $('#user-zip')
-var radius = $('#user-radius')
+// var zipCode = $('#user-zip')
+// var radius = $('#user-radius')
+//Parameters for storeID API call
+var zipCode = 91367
+var radius = 20
 
 var queryURL =
   'https://www.walmart.com/store/finder/electrode/api/stores?singleLineAddr=' +
@@ -11,8 +14,10 @@ var queryURL =
 $.ajax({
   url: queryURL,
   method: 'GET',
-}).then(function (response) {})
-
-$('#submit-button').click(function () {
-  $('#user-zip')
+}).then(function (response) {
+  var returnedStores = response.payload.storesData.stores
+  for (i = 0; i < returnedStores.length; i++) {
+    var storeID = returnedStores[i].id
+    console.log(storeID)
+  }
 })
