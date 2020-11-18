65506346f4ea5cfa9786fc0207cb7cfffc5b2efb
Van Taymany <vtaymany@gmail.com>
2020-11-17 23:11:39 -0800

    displays product details in the DOM

--- assets/js/walmart.js
+++ assets/js/walmart.js
@@ -18,7 +18,6 @@ $.ajax({
   var returnedStores = response.payload.storesData.stores
   for (i = 0; i < returnedStores.length; i++) {
     var storeID = returnedStores[i].id
-    console.log(storeID)
     var upc = 681131381284
     var productQueryURL =
       'https://search.mobile.walmart.com/v1/products-by-code/UPC/' +
@@ -29,8 +28,39 @@ $.ajax({
       url: productQueryURL,
       method: 'GET',
     }).then(function (response) {
-      var availability = response.data.inStore.inventory
-      console.log(availability)
+      var availability = response.data.inStore.inventory.available
+      var productInfo = response.data
+      var productImg = response.data.common.productImageUrl
+      var productTitle = response.data.common.name
+      var productPrice = '$' + response.data.inStore.price.priceInCents / 100
+      var productURL = response.data.common.productUrl
+
+      if (availability) {
+        console.log(productInfo)
+        var cell = $('<div class="cell" data-category="hand-sanitizer"></div>')
+        cell.appendTo('#product-container')
+
+        var img = $('<img class="thumbnail product-thumbnail"/>') //Equivalent: $(document.createElement('img'))
+        img.attr('src', productImg)
+        img.appendTo(cell)
+
+        var title = $('<h5 class="product-title"></h5>').append(productTitle)
+        title.appendTo(cell)
+
+        var price = $('<p class="product-price"></p>').append(productPrice)
+        price.appendTo(cell)
+
+        var buyNow = $('<a class="button" href="">Buy Now</a>')
+        buyNow.attr('href', productURL)
+        buyNow.appendTo(cell)
+      }
     })
   }
 })
+
+// <div class="cell" data-category="hand-sanitizer">
+//   <img class="thumbnail product-thumbnail" src="https://placehold.it/500x600" />
+//   <h5 class="product-title">Nulla At Nulla Justo, Eget</h5>
+//   <p class="product-price">$400</p>
+//   <a href="#" class="button expanded product-url">Buy</a>
+// </div>
