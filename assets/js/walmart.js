5b23e3fb000b375b756b5e7e5bc383f2f5f2ba17
Van Taymany <vtaymany@gmail.com>
2020-11-19 14:36:30 -0800

    displays store and location

--- assets/js/walmart.js
+++ assets/js/walmart.js
@@ -1,55 +1,56 @@
-// The below code fills in the first row of the table
-// var zipCode = $('#user-zip')
-// var radius = $('#user-radius')
-//Parameters for storeID API call
+// Takes user form inputs..
 var zipCode = 91367
 var radius = 20
-
+//..and inserts them into the API url.
 var storeIDQueryURL =
   'https://www.walmart.com/store/finder/electrode/api/stores?singleLineAddr=' +
   zipCode +
   '&serviceTypes=pharmacy&distance=' +
   radius
-
+//Makes the API call to get storeID's..
 $.ajax({
   url: storeIDQueryURL,
   method: 'GET',
-}).then(function (response) {
-  var returnedStores = response.payload.storesData.stores
+}).then(function (storeResponse) {
+  //..and takes those returned storeID's..
+  var returnedStores = storeResponse.payload.storesData.stores
   for (i = 0; i < returnedStores.length; i++) {
     var storeID = returnedStores[i].id
-    var upc = 681131381284
+    const storeName = returnedStores[i].displayName
+    var upc = '711719541080'
     var productQueryURL =
       'https://search.mobile.walmart.com/v1/products-by-code/UPC/' +
       upc +
       '?storeId=' +
       storeID
+    //..and makes the API call for product info.
     $.ajax({
       url: productQueryURL,
       method: 'GET',
-    }).then(function (response) {
-      var availability = response.data.inStore.inventory.available
-      var productInfo = response.data
-      var productImg = response.data.common.productImageUrl
-      var productTitle = response.data.common.name
-      var productPrice = '$' + response.data.inStore.price.priceInCents / 100
-      var productURL = response.data.common.productUrl
-
+    }).then(function (productResponse) {
+      //..and pushes the product info into the DOM
+      var availability = productResponse.data.inStore.inventory.available
+      var productImg = productResponse.data.common.productImageUrl
+      var productTitle = productResponse.data.common.name
+      var productPrice =
+        '$' + productResponse.data.inStore.price.priceInCents / 100
+      var productURL = productResponse.data.common.productUrl
       if (availability) {
-        console.log(productInfo)
-        var cell = $('<div class="cell" data-category="hand-sanitizer"></div>')
+        var cell = $('<div class="cell" data-attribute="ps5-controller"></div>')
         $('#product-container').append(cell)
-
-        var img = $('<img class="thumbnail product-thumbnail"/>') //Equivalent: $(document.createElement('img'))
+        var img = $('<img class="thumbnail product-thumbnail"/>')
         img.attr('src', productImg)
         img.appendTo(cell)
-
         var title = $('<h5 class="product-title"></h5>').append(productTitle)
         title.appendTo(cell)
-
         var price = $('<p class="product-price"></p>').append(productPrice)
         price.appendTo(cell)
-
+        var storeLogo = $(
+          '<img class="store-logo" src="./assets/store-icons/walmart-product-logo.png" alt="Girl in a jacket" width="100px">'
+        )
+        storeLogo.appendTo(cell)
+        var location = $('<p class="product-location"></p>').append(storeName)
+        location.appendTo(cell)
         var buyNow = $('<a class="button" href="">Buy Now</a>')
         buyNow.attr('href', productURL)
         buyNow.appendTo(cell)
@@ -57,10 +58,3 @@ $.ajax({
     })
   }
 })
-
-// <div class="cell" data-category="hand-sanitizer">
-//   <img class="thumbnail product-thumbnail" src="https://placehold.it/500x600" />
-//   <h5 class="product-title">Nulla At Nulla Justo, Eget</h5>
-//   <p class="product-price">$400</p>
-//   <a href="#" class="button expanded product-url">Buy</a>
-// </div>
